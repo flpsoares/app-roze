68f4d2a333380df102fe9ab5d0701a4ec479aa11
Filipe Soares <filipeseventeen1@gmail.com>
2024-01-28 21:53:03 -0300

    Alterado assets

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://app.rozeapp.com.br/backend'
+  baseURL: 'https://backendserver.cloud/roze'
+  // baseURL: 'https://app.rozeapp.com.br/backend'
 })
